82a85876e660123835ece5898b89b42692f18b28
Michael Dyrynda <michael@dyrynda.com.au>
2024-03-19 09:24:19 +1030

    fluent expectations

--- tests/NullableFieldsTest.php
+++ tests/NullableFieldsTest.php
@@ -10,29 +10,29 @@ beforeEach(function () {
 });
 
 it('identifies an empty string as null', function () {
-    expect($this->nullable->nullIfEmpty(''))->toBeNull();
+    expect($this->nullable)->nullIfEmpty('')->toBeNull();
 });
 
 it('identifies a string with whitespace only as null', function () {
-    expect($this->nullable->nullIfEmpty(' '))->toBeNull();
+    expect($this->nullable)->nullIfEmpty(' ')->toBeNull();
 });
 
 it('does not modify a non empty value', function () {
-    expect($this->nullable->nullIfEmpty('michaeldyrynda'))->toBe('michaeldyrynda');
+    expect($this->nullable)->nullIfEmpty('michaeldyrynda')->toBe('michaeldyrynda');
 });
 
 it('does not modify a non empty value surrounded by whitespace', function () {
-    expect($this->nullable->nullIfEmpty(' michaeldyrynda '))->toBe(' michaeldyrynda ');
+    expect($this->nullable)->nullIfEmpty(' michaeldyrynda ')->toBe(' michaeldyrynda ');
 });
 
 it('does not modify a false value', function () {
-    expect($this->nullable->nullIfEmpty(false))->toBeFalse();
+    expect($this->nullable)->nullIfEmpty(false)->toBeFalse();
 });
 
 it('correctly handles an empty array as null', function () {
-    expect($this->nullable->nullIfEmpty([]))->toBeNull();
+    expect($this->nullable)->nullIfEmpty([])->toBeNull();
 });
 
 it('correctly handles a non empty array', function () {
-    expect($this->nullable->nullIfEmpty(['twitter' => 'michaeldyrynda']))->toBe(['twitter' => 'michaeldyrynda']);
+    expect($this->nullable)->nullIfEmpty(['twitter' => 'michaeldyrynda'])->toBe(['twitter' => 'michaeldyrynda']);
 });
